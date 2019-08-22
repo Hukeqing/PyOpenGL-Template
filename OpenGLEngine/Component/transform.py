8763072f1134c46932453ca83bf3ce660864f70e
Mauve <keqing.hu@icloud.com>
2019-08-22 08:57:12 +0800

    尝试使用Vector3代替glm

--- OpenGLEngine/Component/transform.py
+++ OpenGLEngine/Component/transform.py
@@ -11,16 +11,34 @@ class Transform(ComponentManager):
         self.position = position
         self.rotation = rotation
         self.scale = scale
+
         if self.position is None:
             self.position = glm.vec3(0, 0, 0)
+        elif isinstance(self.position, Vector3):
+            self.position = glm.vec3(self.position.get_value())
+        else:
+            self.position = glm.vec3(*self.position)
+
         if self.rotation is None:
             self.rotation = glm.vec3(0, 0, 0)
+        elif isinstance(self.rotation, Vector3):
+            self.rotation = glm.vec3(self.rotation.get_value())
+        else:
+            self.rotation = glm.vec3(*self.rotation)
+
         if self.scale is None:
             self.scale = glm.vec3(1, 1, 1)
+        elif isinstance(self.scale, Vector3):
+            self.scale = glm.vec3(self.scale.get_value())
+        else:
+            self.scale = glm.vec3(*self.scale)
 
+        self.glm_forward = None
+        self.glm_left = None
+        self.glm_up = None
         self.forward = None
-        self.up = None
         self.left = None
+        self.up = None
         self.get_forward()
 
         self.position_lock_alive = (False, False, False)
@@ -34,13 +52,13 @@ class Transform(ComponentManager):
         self.rotation_lock_z = None
 
     def set_position_clock(self, x_clock=None, y_clock=None, z_clock=None):
-        self.position_lock_alive = glm.vec3(x_clock is not None, y_clock is not None, z_clock is not None)
+        self.position_lock_alive = (x_clock, y_clock, z_clock)
         self.position_lock_x = x_clock
         self.position_lock_y = y_clock
         self.position_lock_z = z_clock
 
     def set_rotation_clock(self, x_clock=None, y_clock=None, z_clock=None):
-        self.rotation_lock_alive = glm.vec3(x_clock is not None, y_clock is not None, z_clock is not None)
+        self.rotation_lock_alive = (x_clock, y_clock, z_clock)
         self.rotation_lock_x = x_clock
         self.rotation_lock_y = y_clock
         self.rotation_lock_z = z_clock
@@ -71,18 +89,22 @@ class Transform(ComponentManager):
             self.rotation.z = math_f.clamp(self.rotation.z, *self.rotation_lock_z)
 
     def get_forward(self):
-        self.forward = glm.vec3(0)
-        self.forward.x = -np.sin(glm.radians(self.rotation.y))
-        self.forward.y = np.sin(glm.radians(self.rotation.x))
-        self.forward.z = np.cos(glm.radians(self.rotation.x)) * np.cos(glm.radians(self.rotation.y))
+        self.glm_forward = glm.vec3(0)
+        self.glm_forward.x = -np.sin(glm.radians(self.rotation.y))
+        self.glm_forward.y = np.sin(glm.radians(self.rotation.x))
+        self.glm_forward.z = np.cos(glm.radians(self.rotation.x)) * np.cos(glm.radians(self.rotation.y))
         # self.forward.x = np.cos(glm.radians(self.rotation.x)) * np.cos(glm.radians(self.rotation.y))
         # self.forward.y = np.sin(glm.radians(self.rotation.x))
         # self.forward.z = np.cos(glm.radians(self.rotation.x)) * np.sin(glm.radians(self.rotation.y))
 
-        self.forward = glm.normalize(self.forward)
-        self.left = glm.normalize(glm.cross(glm.vec3(0, 1, 0), self.forward))
-        self.up = glm.normalize(glm.cross(self.forward, self.left))
+        self.glm_forward = glm.normalize(self.glm_forward)
+        self.glm_left = glm.normalize(glm.cross(glm.vec3(0, 1, 0), self.glm_forward))
+        self.glm_up = glm.normalize(glm.cross(self.glm_forward, self.glm_left))
+
+        self.forward = Vector3(self.glm_forward)
+        self.left = Vector3(self.glm_left)
+        self.up = Vector3(self.glm_up)
 
     def get_view_matrix(self):
         self.get_forward()
-        return glm.lookAt(self.position, self.position + self.forward, self.up)
+        return glm.lookAt(self.position, self.position + self.glm_forward, self.glm_up)
