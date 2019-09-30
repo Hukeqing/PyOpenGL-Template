4a14d489dfae2083aadd5dd2a32963a546418841
Mauve <keqing.hu@icloud.com>
2019-09-30 08:04:17 +0800

    add ceil and clamp01 and lerp

--- OpenGLEngine/Function/math_f.py
+++ OpenGLEngine/Function/math_f.py
@@ -2,11 +2,25 @@ import glm
 import numpy as np
 
 
-def clamp(value, value_min, value_max):
+def ceil(value):
+    if int(value) == value:
+        return value
+    return int(value) + 1
+
+
+def clamp(value, value_min=None, value_max=None):
     if value_max is not None and value > value_max:
         return value_max
     if value_min is not None and value < value_min:
         return value_min
+    return value
+
+
+def clamp01(value):
+    if value < 0:
+        return 0
+    if value > 1:
+        return 1
     return value
 
 
@@ -26,6 +40,6 @@ def tan(*args, **kwargs):
     return np.tan(*args, **kwargs)
 
 
-def Lerp(begin, end, value):
+def lerp(begin, end, value):
     value = clamp(value, 0, 1)
     return begin + (end - begin) * value
